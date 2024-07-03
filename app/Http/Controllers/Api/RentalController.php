e6b04bc1e4d0d20fa5e1cb1fac6163c2141d2c0c
skyandfly <skyandy2796@yandex.ru>
2024-07-04 00:16:51 +0300

    feat: add rental store function

--- app/Http/Controllers/Api/RentalController.php
+++ app/Http/Controllers/Api/RentalController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Rental;
 use Illuminate\Http\Request;
 
 class RentalController extends Controller
 {
+
     public function store(Request $request)
     {
+        $data = $request->validate([
+            'car_id' => 'required|exists:car_models,id',
+            'user_id' => 'required|exists:users,id',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'duration' => 'required',
+        ]);
 
+        return Rental::create($data);
     }
 }
--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Rental extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class, 'car_id');
+    }
 }
